be699268a963d3862a07339a82a448d2e45617c1
NicolasReyes1 <107068872+NicolasReyes1@users.noreply.github.com>
2022-06-07 13:45:35 -0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero 1; ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numero 2; ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Menu");
+            Console.WriteLine("1 sumar");
+            Console.WriteLine("2 restar");
+            Console.WriteLine("3 multiplicar");
+            string opc = Console.ReadLine();
+            if(opc == "1"){
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            }else if  (opc == "2")   {
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            }else if  (opc == "3")   {
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+            }else {
+            Console.WriteLine("Opcion incorrecta");
+            } 
+            Console.ReadKey();
+        }
+    }
+}
